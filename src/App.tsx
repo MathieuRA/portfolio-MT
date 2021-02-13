c56672e2cbb58145f19e0c9bae6dc27cc5ed5894
mathieuRA <contact@mathieu-raisin.fr>
2021-02-13 14:13:14 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <p>Test</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
