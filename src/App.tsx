aab91d7383dcd68468b36f84608dc91057a2e1ab
mathieuRA <contact@mathieu-raisin.fr>
2021-02-21 15:19:11 +0100

    creation of the customScroll. Need to fix it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Data } from './utils'
 import { Menu, Page } from './components'
 
 import './app.css'
+import { throttle } from 'lodash'
 
 const data = Data.getInstance()
 const sliderImgs = data.getSliderImgs()
+const menuItem = data.getMenuItems()
+
+let navIterator = 0
+
+const customScroll = (e: WheelEvent): void => {
+  const down = e.deltaY > 1
+  if (
+    (down && navIterator === menuItem.length - 1) ||
+    (!down && navIterator === 0)
+  ) {
+    return
+  }
+  e.deltaY > 1 ? (navIterator += 1) : (navIterator -= 1)
+  window.location.hash = menuItem[navIterator]
+}
 
 function App() {
+  useEffect(() => {
+    window.addEventListener(
+      'wheel',
+      throttle(customScroll, 1000, {
+        leading: true,
+        trailing: false,
+      })
+    )
+    return () => {
+      window.removeEventListener(
+        'wheel',
+        throttle(() => {}, 1000, {
+          leading: true,
+          trailing: false,
+        })
+      )
+    }
+  }, [])
   return (
     <div className='App'>
       <Menu itemsNavigation={data.getNavigation()} />
--- src/components/templates/Page/Slider.tsx
+++ src/components/templates/Page/Slider.tsx
@@ -1,23 +1,27 @@
 import Carousel from 're-carousel'
-
 import { FC } from 'react'
 
 import './page.css'
+import './slider.css'
 
 const STYLE_EMBED = {
   height: '100%',
 }
 
 interface PropsSlider {
+  anchor: string
   imgs: string[]
 }
-const Slider: FC<PropsSlider> = ({ imgs }) => {
+const Slider: FC<PropsSlider> = ({ anchor, imgs }) => {
+  // Auto have to be dynamic. Maybe create a hook ?
+  // We are reusing a lot if the component are active.
+  // So instead of repeat the code, better to create something to reuse
   return (
     <div className='slider'>
       <Carousel
-        auto
+        auto={window.location.hash === `#${anchor}`}
         axis={'x'}
-        duration={700}
+        duration={1000}
         loop
         interval={5000}
       >
@@ -33,4 +37,5 @@ const Slider: FC<PropsSlider> = ({ imgs }) => {
     </div>
   )
 }
+
 export default Slider
