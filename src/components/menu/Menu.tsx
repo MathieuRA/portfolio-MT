4e10e5682119d2d322a29e2598b4633eb79bbe1d
mathieuRA <contact@mathieu-raisin.fr>
2021-03-06 18:53:03 +0100

    creation of the menu list in mobile

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,9 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+import { Link } from '../templates'
 
 import { IMenu } from '../../interfaces'
-import { Link } from '../templates'
 
 import './menu.css'
 
@@ -33,6 +35,7 @@ interface PropsMenu {
   itemsNavigation: IMenu
 }
 const Menu: FC<PropsMenu> = ({ itemsNavigation }) => {
+  const listCollapsedMenu = useRef<HTMLDivElement>(null)
   const isMobile = window.innerWidth <= 1024
   const {
     contact,
@@ -42,29 +45,58 @@ const Menu: FC<PropsMenu> = ({ itemsNavigation }) => {
   } = itemsNavigation
   const { alt, src } = logo
 
+  const toggleMenuList = (e: HTMLDivElement | any) => {
+    if (e instanceof HTMLDivElement) {
+      e.style.opacity === '0' && (e.style.display = 'block')
+      return
+    }
+    e.target.style.opacity === '0'
+      ? (e.target.style.display = 'none')
+      : (e.target.style.display = 'block')
+  }
+
   return (
-    <nav id='menu'>
-      {isMobile ? (
-        <CollapseMenu logo={logo} />
-      ) : (
-        <>
-          <MenuSection position={'left'} items={leftPart}>
-            <img
-              alt={alt}
-              src={src}
-              style={{ width: 110, height: 'auto' }}
-            />
-          </MenuSection>
-          <MenuSection position={'right'} items={rightPart}>
-            <Link
-              label={contact.toUpperCase()}
-              anchor
-              to={contact}
-            />
-          </MenuSection>
-        </>
+    <>
+      {isMobile && (
+        <div
+          id='listMenu'
+          onTransitionEnd={toggleMenuList}
+          ref={listCollapsedMenu}
+          style={{
+            opacity: 0,
+          }}
+        ></div>
       )}
-    </nav>
+      <nav id='menu'>
+        {isMobile ? (
+          <CollapseMenu
+            itemsNavigation={itemsNavigation}
+            portal={listCollapsedMenu.current}
+            toggleMenuList={toggleMenuList}
+          />
+        ) : (
+          <>
+            <MenuSection position={'left'} items={leftPart}>
+              <img
+                alt={alt}
+                src={src}
+                style={{ width: 110, height: 'auto' }}
+              />
+            </MenuSection>
+            <MenuSection
+              position={'right'}
+              items={rightPart}
+            >
+              <Link
+                label={contact.toUpperCase()}
+                anchor
+                to={contact}
+              />
+            </MenuSection>
+          </>
+        )}
+      </nav>
+    </>
   )
 }
 
@@ -115,20 +147,45 @@ const MenuSection: FC<MenuSectionProps> = ({
 }
 
 interface PropsCollpaseMenu {
-  logo: {
-    alt: string
-    src: string
-  }
+  itemsNavigation: IMenu
+  portal: HTMLDivElement | null
+  toggleMenuList: Function
 }
-const CollapseMenu: FC<PropsCollpaseMenu> = ({ logo }) => {
+const CollapseMenu: FC<PropsCollpaseMenu> = ({
+  itemsNavigation,
+  portal,
+  toggleMenuList,
+}) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
+  useEffect(() => {
+    portal?.addEventListener('touchmove', (e) => {
+      e.preventDefault()
+    })
+  }, [])
 
+  const {
+    contact,
+    leftPart,
+    rightPart,
+    logo,
+  } = itemsNavigation
   const { alt, src } = logo
   const { bars, close, open } = BURGERMENUCONFIG
 
+  const fullMenuItems = [...leftPart, ...rightPart, contact]
+
   const toggleMenu = () => {
+    toggleMenuList(portal)
+    if (portal) {
+      menuIsOpen
+        ? (portal.style.opacity = '0')
+        : setTimeout(() => {
+            portal.style.opacity = '1'
+          }, 50)
+    }
     setMenuIsOpen(!menuIsOpen)
   }
+
   return (
     <>
       <img
@@ -150,6 +207,24 @@ const CollapseMenu: FC<PropsCollpaseMenu> = ({ logo }) => {
           />
         ))}
       </div>
+      {portal !== null &&
+        createPortal(
+          <ul>
+            {fullMenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link
+                    anchor
+                    HTMLClass='itemBurgerMenu'
+                    label={item.toLocaleUpperCase()}
+                    to={item}
+                  />
+                </li>
+              )
+            })}
+          </ul>,
+          portal
+        )}
     </>
   )
 }
--- src/components/templates/link/Link.tsx
+++ src/components/templates/link/Link.tsx
@@ -2,17 +2,21 @@ import { FC } from 'react'
 
 interface PropsLink {
   anchor?: boolean
+  HTMLClass?: string
   label: string
   to: string
 }
 const Link: FC<PropsLink> = ({
   anchor = false,
+  HTMLClass,
   label,
   to,
 }) => {
   return (
     <a
-      className={`customLink`}
+      className={`customLink ${
+        HTMLClass !== undefined ? HTMLClass : ''
+      }`}
       href={anchor ? `#${to}` : to}
     >
       {label}
