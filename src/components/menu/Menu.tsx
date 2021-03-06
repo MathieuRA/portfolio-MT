9e566ebe08ee11eb323130d666da638dce081706
mathieuRA <contact@mathieu-raisin.fr>
2021-03-06 14:57:18 +0100

    create burger menu and animation

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,14 +1,39 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { IMenu } from '../../interfaces'
 import { Link } from '../templates'
 
 import './menu.css'
 
+const BURGERMENUCONFIG = {
+  close: {
+    backgroundColor: 'var(--main-gold)',
+    opacity: '1',
+    transform: 'rotate(0deg)',
+  },
+  open: {
+    backgroundColor: 'var(--main-grey)',
+  },
+  bars: [
+    {
+      transform: 'rotate(400deg)',
+      top: '13px',
+    },
+    {
+      transform: 'rotate(320deg)',
+    },
+    {
+      opacity: '0',
+      transform: 'rotate(360deg)',
+    },
+  ],
+}
+
 interface PropsMenu {
   itemsNavigation: IMenu
 }
 const Menu: FC<PropsMenu> = ({ itemsNavigation }) => {
+  const isMobile = window.innerWidth <= 1024
   const {
     contact,
     leftPart,
@@ -19,20 +44,26 @@ const Menu: FC<PropsMenu> = ({ itemsNavigation }) => {
 
   return (
     <nav id='menu'>
-      <MenuSection position={'left'} items={leftPart}>
-        <img
-          alt={alt}
-          src={src}
-          style={{ width: 110, height: 'auto' }}
-        />
-      </MenuSection>
-      <MenuSection position={'right'} items={rightPart}>
-        <Link
-          label={contact.toUpperCase()}
-          anchor
-          to={contact}
-        />
-      </MenuSection>
+      {isMobile ? (
+        <CollapseMenu logo={logo} />
+      ) : (
+        <>
+          <MenuSection position={'left'} items={leftPart}>
+            <img
+              alt={alt}
+              src={src}
+              style={{ width: 110, height: 'auto' }}
+            />
+          </MenuSection>
+          <MenuSection position={'right'} items={rightPart}>
+            <Link
+              label={contact.toUpperCase()}
+              anchor
+              to={contact}
+            />
+          </MenuSection>
+        </>
+      )}
     </nav>
   )
 }
@@ -80,6 +111,46 @@ const MenuSection: FC<MenuSectionProps> = ({
         </>
       )}
     </div>
+  )
+}
+
+interface PropsCollpaseMenu {
+  logo: {
+    alt: string
+    src: string
+  }
+}
+const CollapseMenu: FC<PropsCollpaseMenu> = ({ logo }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  const { alt, src } = logo
+  const { bars, close, open } = BURGERMENUCONFIG
+
+  const toggleMenu = () => {
+    setMenuIsOpen(!menuIsOpen)
+  }
+  return (
+    <>
+      <img
+        alt={alt}
+        src={src}
+        style={{
+          width: 110,
+        }}
+      />
+      <div id='burgerMenu' onClick={toggleMenu}>
+        {bars.map((cssProperty, index) => (
+          <div
+            key={index}
+            style={
+              menuIsOpen
+                ? { ...cssProperty, ...open }
+                : { ...close }
+            }
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
