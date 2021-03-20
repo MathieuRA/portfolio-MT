6d912a46b9095ca5038589966755419863c15068
mathieuRA <contact@mathieu-raisin.fr>
2021-03-20 16:44:39 +0100

    Add service worker and update logo

---
+++ src/components/clapperboard/Clapperboard.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react'
+
+import './clapperboard.css'
+// import './clapperboard.js'
+
+const Clapperboard = () => {
+  const stick = useRef<HTMLDivElement>(null)
+  if (stick && stick.current) {
+    //stick.current.style.transform = 'rotate(330deg)'
+  }
+
+  return (
+    <div className='clapper'>
+      <div className='clapper-container'>
+        <div className='clapper-body'>
+          <div className='top'>
+            <div className='stick stick1' ref={stick}>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+            </div>
+            <div className='stick stick2'>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+              <div className='rect'></div>
+            </div>
+          </div>
+          <div className='bottom'></div>
+        </div>
+        <div className='circle'></div>
+        <div className='play'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Clapperboard
