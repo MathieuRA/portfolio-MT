26f7b4b17ae0e5a3dc09112c5497a1e199a8b963
mathieuRA <contact@mathieu-raisin.fr>
2021-03-26 19:15:20 +0100

    handle update of pwa

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,19 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    alert(
+      "Nouvelle version disponible. L'application va recharger."
+    )
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({
+        type: 'SKIP_WAITING',
+      })
+    }
+    window.location.reload()
+  },
+})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
